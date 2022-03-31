e1506fdff6fbbfd080d0b95420b7e40cb9b4e6ae
DnyanadaD <dnyanada.dhore@gmail.com>
2022-03-31 19:46:11 +0530

    "Handon Added"

---
+++ C#/File_Han_Html/FileHandlingHTML.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_Handle_HTML
+{
+    class FileHandlingHTML
+    {
+        public void ReadFile()
+        {
+            FileStream fileStream = new FileStream(@"C:\Capg_Training\Sql\Training_Capg\C#\File_Han_Html\mydata.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fileStream);
+            int count = 0;
+            string Word = "<input";
+            while (reader.Peek() > 0)
+            {
+                string line = reader.ReadLine();
+                if (line.Contains(Word))
+                {
+                    Console.WriteLine(line);
+                    count++;
+                }
+            }
+            Console.WriteLine("Number of <input = " + count);
+            reader.Close();
+            fileStream.Close();
+        }
+    }
+}
---
+++ C#/File_Han_Html/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_Handle_HTML
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            FileHandlingHTML fileHandling = new FileHandlingHTML();
+            //fileHandling.WriteFile();
+            fileHandling.ReadFile();
+            Console.Read();
+        }
+    }
+}
+
