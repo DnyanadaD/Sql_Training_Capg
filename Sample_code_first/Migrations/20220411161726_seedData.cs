981dd2d02514139467f4b2dfaf8f37b85c1fd6cf
DnyanadaD <dnyanada.dhore@gmail.com>
2022-04-11 22:00:47 +0530

    "Handson-codeFirst added"

---
+++ Sample_code_first/Migrations/20220411161726_seedData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sample_code_first.Migrations
+{
+    public partial class seedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 1L, "In meeting need to discuss some points", true, "Meet with management" });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 2L, "List of this this item buy", false, "Go for Shopping" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 2L);
+        }
+    }
+}
---
+++ Sample_code_first/Models/TodoContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Sample_code_first.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItem { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<TodoItem>().HasData(
+                new TodoItem() { Id = 1, Name = "Meet with management", IsComplete = true, Description = "In meeting need to discuss some points" },
+                new TodoItem() { Id = 2, Name = "Go for Shopping", IsComplete = false, Description = "List of this this item buy" }
+                );
+        }
+
+    
+    }
+}
